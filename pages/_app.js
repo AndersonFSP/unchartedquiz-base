1423e3eeaf8d834102399d906c0796e90def1091
Anderson Filipe <anderson.lemobs@gmail.com>
2021-02-09 21:47:01 -0300

    Primeiro commit aula1

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,38 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
+* {
+  box-sizing: border-box;
 }
 
+body {
+    margin:0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', 'sans-serif';
+    color: ${({ theme }) => theme.colors.contrastText}
+}
+
+html, body {
+    min-height: 100vh;
+}
+
+#__next {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+}
+`
+db
+const theme = db.theme;
+const backgroundImage = db.bg;
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <ThemeProvider theme={theme}> 
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
